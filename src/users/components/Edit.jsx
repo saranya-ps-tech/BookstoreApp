e4e5c3f817a04b8d564a11dd3f57b0f73f143058
Saranya P S <saranyasasikumar619@gmail.com>
2025-11-24 18:35:38 +0530

    proje ct completed

--- src/users/components/Edit.jsx
+++ src/users/components/Edit.jsx
@@ -96,7 +96,7 @@ function Edit() {
 
   return (
     <>
-      <button onClick={() => setOffCanvasStatus(true)} className="text-blue-600 border border-blue-600 rounded p-3 hover:bg-blue-600 text-white"><FontAwesomeIcon
+      <button onClick={() => setOffCanvasStatus(true)} className="text-blue-600 border border-blue-600 rounded p-3 hover:bg-blue-600 hover:text-white"><FontAwesomeIcon
         icon={faPenToSquare} /> Edit</button>
       {/* offCanvas */}
       {offCanvasStatus &&
